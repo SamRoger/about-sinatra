761709d9163ccdb1f83170b3043b3ef2ae8d823b
Sam Almeida <samRAlmeida@icloud.com>
2017-05-27 20:03:48 -0400

    skill point set

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,14 @@
-var rightSide = document.getElementsByClassName("right-side")[0];
-var picture = document.getElementsByClassName("sam-picture")[0];
+var rightSide = document.getElementsByClassName("right-background")[0];
+var leftSide = document.getElementsByClassName("left-background")[0];
 
 window.addEventListener("scroll", function() {
-	if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
-		rightSide.classList.add = 'grayScale'
-		picture.classList.add = 'grayScale'
+	if (document.body.scrollTop > 275 || document.documentElement.scrollTop > 275) {
+		rightSide.style.filter = "grayscale(100%)"
+		leftSide.style.filter = "grayscale(100%)"
+	} else {
+		rightSide.style.filter = ""
+		leftSide.style.filter = ""
+
 	}
 })
+;
