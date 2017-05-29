ec7c21fb8059b174ef2a01af9ce6e56cbbf50b2f
Sam Almeida <samRAlmeida@icloud.com>
2017-05-29 12:07:27 -0400

    base set for 3rd row

--- public/js/main.js
+++ public/js/main.js
@@ -2,13 +2,52 @@ var rightSide = document.getElementsByClassName("right-background")[0];
 var leftSide = document.getElementsByClassName("left-background")[0];
 
 window.addEventListener("scroll", function() {
-	if (document.body.scrollTop > 275 || document.documentElement.scrollTop > 275) {
-		rightSide.style.filter = "grayscale(100%)"
-		leftSide.style.filter = "grayscale(100%)"
+	if (document.body.scrollTop > 230 || document.documentElement.scrollTop > 230) {
+		rightSide.style.filter = 'grayscale(100%)'
+		leftSide.style.filter = 'grayscale(100%)'
 	} else {
 		rightSide.style.filter = ""
 		leftSide.style.filter = ""
-
 	}
 })
-;
+
+var menuBackground2 = document.getElementsByClassName("menu-background2")[0];
+
+window.addEventListener("scroll", function() {
+	if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+		menuBackground2.style.backgroundColor = 'rgb(168,86,79)'
+	} else {
+		menuBackground2.style.backgroundColor = ''
+	}
+})
+
+
+var skillsContainer = document.getElementsByClassName("skills-container")[0];
+
+window.addEventListener("scroll", function() {
+	if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
+		skillsContainer.style.animation = 'fadeinOpacity 10s forwards'
+		console.log("hi")
+	} else {
+		skillsContainer.style.animation = ''	
+	}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
