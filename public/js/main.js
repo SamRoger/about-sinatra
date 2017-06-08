4821f0feb34b10fdc2b1b8fc1a2d0a88baac6e85
Sam Almeida <samRAlmeida@icloud.com>
2017-06-08 16:35:57 -0400

    uses callback func

--- public/js/main.js
+++ public/js/main.js
@@ -1,59 +1,50 @@
 
 
-// var rightSide = document.getElementsByClassName("right-background")[0];
-// var leftSide = document.getElementsByClassName("left-background")[0];
+ var rightSide = document.getElementsByClassName("right-background")[0];
+ var leftSide = document.getElementsByClassName("left-background")[0];
  var menuBackground2 = document.getElementsByClassName("menu-background2")[0];
-// var skillsContainer = document.getElementsByClassName("skills-container")[0];
-// // ==============================================================
-// window.addEventListener("scroll", parallax);
-// function parallax() {
-// var parallax = document.getElementsByClassName("parallax")[0];
-// 	ypos = window.pageYOffset;
-// 	parallax.style.top = ypos * .5 + "px";
-// }
-// // ==============================================================
-// // ==============================================================
-// window.addEventListener("scroll", test);
-// function test() {
-
-// 	if (window.pageYOffset > 100) {
-// 		menuBackground2.style.animation = 'menu-bg-fade 2s forwards'
-		
-// 		console.log("down over 100")
-// }  else {
-// 	if (window.pageYOffset < 100) {
-// 		menuBackground2.style.animation = 'menu-bg-fade-out1 2s forwards'
-// 		console.log("up under 100")
-// 	}
-// 		console.log("up")
-// 	}
-// 		scroll = window.pageYOffset
-// }
-
-var quit = false
+ var skillsContainer = document.getElementsByClassName("skills-container")[0];
 // ==============================================================
-var display = document.getElementsByClassName("display")[0];
-var scroll = window.pageYOffset;
-display.innerHTML = scroll
+window.addEventListener("scroll", parallax);
+function parallax() {
+var parallax = document.getElementsByClassName("parallax")[0];
+	ypos = window.pageYOffset;
+	parallax.style.top = ypos * .5 + "px";
+}
+
 // ==============================================================
-window.addEventListener("scroll", test2);
-function test2() {
+window.addEventListener("scroll", function() {
+ var display = document.getElementsByClassName("display")[0];
+ var scroll = window.pageYOffset;
+ display.innerHTML = scroll
+})
+// ==============================================================
+window.addEventListener("scroll", grayScale);
+function grayScale() {
 	if (window.pageYOffset > 100) {
-		menuBackground2.style.animation = 'menu-bg-fade 1s forwards'
+		rightSide.style.animation = 'grayScale 2s forwards'
+		leftSide.style.animation = 'grayScale 2s forwards'
 		} else if (window.pageYOffset < 100) {
-				scrollUp();
+				colorBack();
 		}
 }
-
-
-window.addEventListener("scroll", scrollUp);
-var scrollUp = function scrollUp() {
+	var colorBack = function colorBack() {
+		rightSide.style.animation = 'colorIn 2s forwards'
+		leftSide.style.animation = 'colorIn 2s forwards'
+}
+// ==============================================================
+window.addEventListener("scroll", menuBgFade);
+function menuBgFade() {
+	if (window.pageYOffset > 720) {
+		menuBackground2.style.animation = 'menu-bg-fade 1s forwards'
+		} else if (window.pageYOffset < 720) {
+				scrollUpMenuFadeBack();
+		}
+}
+var scrollUpMenuFadeBack = function scrollUpMenuFadeBack() {
 		menuBackground2.style.animation = 'menu-bg-fade-out1 1s forwards'
 }
-// // ==============================================================
-
-
-
+// ==============================================================
 
 
 
